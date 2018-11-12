7eeda684ec7c0a1081ff70978b4ff0959fc13f04
Johnny Schaap <Johnny.Schaap@akzonobel.com>
2018-11-12 20:16:11 +0100

    Started on the shopping page

--- src/DEV238x-Capstone.front-end/src/app/app-routing.module.ts
+++ src/DEV238x-Capstone.front-end/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     children: []
   },
   {
-    path: 'browse-products',
+    path: 'shopping',
     component: BrowseProductsComponent,
     children: []
   }, 
--- src/DEV238x-Capstone.front-end/src/app/pages/browse-products/browse-products.component.ts
+++ src/DEV238x-Capstone.front-end/src/app/pages/browse-products/browse-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AssortmentService } from 'src/app/shared/services/assortment-service.service';
+import { Assortment } from 'src/app/shared/models/Assortment';
 
 @Component({
   selector: 'app-browse-products',
@@ -8,14 +9,59 @@ import { AssortmentService } from 'src/app/shared/services/assortment-service.se
 })
 export class BrowseProductsComponent implements OnInit {
 
+  assortment: Assortment[];
+  shownProduct: number = 0;
+  totalProduct: number = 0;
+  selectedSubCatagory: string = "None Selected";
+  sortOptions: string[];
+
+  showInStockOnly: boolean = false;
+  categoryIndex: number;
+  subcategoryIndex: number;
+  selectedSortOption: string;
+
   constructor(private assortmentService: AssortmentService) {
-    debugger;
+
     assortmentService.getAssortment().subscribe((assortment) => {
-      debugger;
+      this.assortment = assortment;
     });
+
+    this.sortOptions = ["None", "Price", "Alphabetically", "Rating"];
+    this.selectedSortOption = this.sortOptions[0];
   }
 
   ngOnInit() {
   }
 
+  changeInStockOnly(event) {
+    this.showInStockOnly = !this.showInStockOnly;
+    this.updateProducts();
+  }
+
+  changeSorting(event) {
+    this.updateProducts();
+  }
+
+  changeSubCategory(categoryIndex: number, subcategoryIndex: number) {
+    if (this.categoryIndex !== undefined && this.subcategoryIndex !== undefined) {
+      this.assortment[this.categoryIndex].subcategories[this.subcategoryIndex].selected = false;
+    }
+
+    this.categoryIndex = categoryIndex;
+    this.subcategoryIndex = subcategoryIndex;
+    this.assortment[this.categoryIndex].subcategories[this.subcategoryIndex].selected = true;
+    this.selectedSubCatagory = this.assortment[this.categoryIndex].subcategories[this.subcategoryIndex].name;
+
+    this.updateProducts();
+  }
+
+  updateProducts() {
+    if (this.categoryIndex === undefined && this.subcategoryIndex === undefined) {
+      return;
+    }
+
+    // TODO
+    this.shownProduct = 2;
+    this.totalProduct = 2;
+  }
 }
