205a66612fdfefe73444fb9dcc7340743d606257
Johnny Schaap <johnny.schaap@stcorp.nl>
2018-11-14 15:48:09 +0100

    Added a unit test

--- src/DEV238x-Capstone.front-end/src/app/shared/services/assortment.service.spec.ts
+++ src/DEV238x-Capstone.front-end/src/app/shared/services/assortment.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AssortmentService } from './assortment.service';
 
-describe('AssortmentServiceService', () => {
+describe('AssortmentService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
--- src/DEV238x-Capstone.front-end/src/app/shared/services/cart.service.spec.ts
+++ src/DEV238x-Capstone.front-end/src/app/shared/services/cart.service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CartService } from './cart.service';
+import { Item } from '../models/item';
 
-describe('CartServiceService', () => {
+describe('CartService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: CartService = TestBed.get(CartService);
     expect(service).toBeTruthy();
   });
+  
+  it('should add item', () => {
+    const service: CartService = TestBed.get(CartService);
+    expect(service).toBeTruthy();
+
+    let item = new Item();
+    item.name = "testName";
+    let addedAmount = 5;
+
+    service.addItem(item, addedAmount);
+
+    expect(service.cart.products[item.name]).not.toBeUndefined();
+    expect(service.cart.products[item.name].amount).toBe(addedAmount);
+  })
 });
